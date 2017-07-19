24bea2f8de59ae60e8ece1cfb9c82b1017aea5c9
iglad <aliyousefi.tec@gmail.com>
2017-07-20 02:10:28 +0430

    change prefix function name to iplug_

---
+++ readme.txt
@@ -0,0 +1,28 @@
+===  woocommerce-product-magnifier  ===
+Contributors: hexboy, gladx
+Donate link: http://www.iplug.ir/donate
+Tags: woocommerce, magnifier
+Tested up to: 4.6
+Requires at least: 3.9
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A simple magnifier for use zoom product picture 
+
+== Description ==
+
+A simple magnifier for use zoom product picture 
+
+Features include:
+
+*	Manual zoom level
+*	Circle or rectangle shape
+
+== Installation ==
+
+You can install woocommerce-product-magnifier  by either using wordpress.org plugin directory, or by directly uploading plugin files to your server.
+
+== Changelog ==
+
+= 0.1 =
--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 /**
  * @package woocommerce-product-magnifier
  * @version 1.0
- */
+ *
 /*
 Plugin Name: woocommerce product magnifier
 Plugin URI: https://github.com/hexboy/woocommerce-product-magnifier
@@ -11,7 +11,7 @@ Author: Hexboy
 Version: 1.0.
 Author URI: https://github.com/hexboy/
  */
-function hex_zoom_product_image() {
+function iplug_zoom_product_image() {
 	global $product, $post;
 	if ( !function_exists( 'is_woocommerce' ) ) return;
 	if ( !is_woocommerce() || !is_product() ) return;
@@ -34,21 +34,20 @@ function hex_zoom_product_image() {
 	<?php
 }
 
-add_action('wp_footer', "hex_zoom_product_image");
+add_action('wp_footer', "iplug_zoom_product_image");
 
 //Remove prettyPhoto lightbox
-add_action('wp_enqueue_scripts', 'hex_remove_woo_lightbox', 99);
-function hex_remove_woo_lightbox() {
+add_action('wp_enqueue_scripts', 'iplug_remove_woo_lightbox', 99);
+function iplug_remove_woo_lightbox() {
 	wp_dequeue_style('woocommerce_prettyPhoto_css');
 	wp_dequeue_script('prettyPhoto');
 	wp_dequeue_script('prettyPhoto-init');
 }
 
-//LAB GLAD
 
-add_action('admin_menu', 'register_iplug_zoom_menu_page');
+add_action('admin_menu', 'iplug_register_zoom_menu_page');
 
-function register_iplug_zoom_menu_page() {
+function iplug_register_zoom_menu_page() {
 	$plugin_dir_path = dirname(__FILE__);
 	add_menu_page('Zoom Image', 'Zoom Image', 'manage_options', 'zoom-image', 'iplug_zoom_image_menu_page', plugins_url('/images/icon.svg', __FILE__), 6);
 }
@@ -68,37 +67,36 @@ function iplug_zoom_image_menu_page() {
 	<?php
 }
 
-//---------------------------
 
-function display_zoom_level() {
+function iplug_display_zoom_level() {
 	?>
     	<input type="text"  type="number" name="iplug_zoom_level" id="iplug_zoom_level" value="<?php echo get_option('iplug_zoom_level',1); ?>" />
     <?php
 }
-function display_zoom_size() {
+function iplug_display_zoom_size() {
 	?>
     	<input type="text" name="iplug_zoom_size" id="iplug_zoom_size" value="<?php echo get_option('iplug_zoom_size',300); ?>" />
     <?php
 }
 
-function display_transparent() {
+function iplug_display_transparent() {
 	?>
     	<input type="checkbox" name="iplug_transparent" value="1" <?php checked(1, get_option('iplug_transparent',1), true);?> />
     <?php
 }
 
-function display_iscircle() {
+function iplug_display_iscircle() {
 	?>
 		<input type="checkbox" name="iplug_iscircle" value="1" <?php checked(1, get_option('iplug_iscircle',1), true);?> />
 	<?php
 }
 
-function display_addon_panel_fields() {
+function iplug_display_addon_panel_fields() {
 	add_settings_section("iplug-zoom-image", "تنظیمات بزرگنمایی تصویر", null, "iplug-zoom");
-	add_settings_field("iplug_zoom_level", "Set Zoom Level", "display_zoom_level", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_zoom_size", "Set Zoom Width", "display_zoom_size", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_transparent", "Transparent", "display_transparent", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_iscircle", "Do you want show Circle ?", "display_iscircle", "iplug-zoom", "iplug-zoom-image");
+	add_settings_field("iplug_zoom_level", "Set Zoom Level", "iplug_display_zoom_level", "iplug-zoom", "iplug-zoom-image");
+	add_settings_field("iplug_zoom_size", "Set Zoom Width", "iplug_display_zoom_size", "iplug-zoom", "iplug-zoom-image");
+	add_settings_field("iplug_transparent", "Transparent", "iplug_display_transparent", "iplug-zoom", "iplug-zoom-image");
+	add_settings_field("iplug_iscircle", "Do you want show Circle ?", "iplug_display_iscircle", "iplug-zoom", "iplug-zoom-image");
 
 	register_setting("iplug-zoom-image", "iplug_zoom_level");
 	register_setting("iplug-zoom-image", "iplug_zoom_size");
@@ -106,14 +104,14 @@ function display_addon_panel_fields() {
 	register_setting("iplug-zoom-image", "iplug_iscircle");
 }
 
-add_action("admin_init", "display_addon_panel_fields");
+add_action("admin_init", "iplug_display_addon_panel_fields");
 
-add_action('admin_head', 'hex_zoom_product_admin_style');
-function hex_zoom_product_admin_style() {
-  echo '<style>
-    li#toplevel_page_zoom-image .wp-menu-image img {
-      height: 100%;
-      padding: 0;
-    }
-  </style>';
+add_action('admin_head', 'iplug_zoom_product_admin_style');
+function iplug_zoom_product_admin_style() {
+  echo	'<style>
+		    li#toplevel_page_zoom-image .wp-menu-image img {
+		      height: 100%;
+		      padding: 0;
+		    }
+		</style>';
 }
