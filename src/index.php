6b2c443a8204f70e45dbb01f2d47b3bfc4a3d3cc
Hassan Yousefi <hexboy2011@gmail.com>
2019-01-29 01:31:55 +0330

    switch to cmb2 for plugin options

--- src/index.php
+++ src/index.php
@@ -9,105 +9,158 @@
  *  Author URI: https://github.com/hexboy/
  */
 
-function iplug_zoom_product_image() {
-	global $product, $post;
-	if ( !function_exists( 'is_woocommerce' ) ) return;
-	if ( !is_woocommerce() || !is_product() ) return;
-	if ( $product->get_image_id() < 1 ) return;
+ /**
+ * Load CMB2 plugin
+ */
+require_once WP_PLUGIN_DIR . '/cmb2/init.php';
 
-	// enqueue style and script
-	wp_enqueue_style('Zoom-Product-Style', plugins_url('/css/Zoom-Product.min.css', __FILE__));
-	wp_enqueue_script('Zoom-Product-Script', plugins_url('/js/Zoom-Product.min.js', __FILE__), array('jquery'), '2.0.0', true);
+add_action('wp_footer', 'ipzo_product_image');
+function ipzo_product_image()
+{
+    global $product, $post;
+    if (!function_exists('is_woocommerce')) {
+        return;
+    }
+    if (!is_woocommerce() || !is_product()) {
+        return;
+    }
+    if ($product->get_image_id() < 1) {
+        return;
+    }
 
-	// get plugin options
-
-	$iscircle    = get_option('iplug_iscircle',1) ? 1 : 0;
-	$transparent = get_option('iplug_transparent',0) ? 1 : 0;
-	$zoom_level  = get_option('iplug_zoom_level',1.2);
-	$zoom_size   = get_option('iplug_zoom_size',300);
-	?>
-	<script type='text/javascript'>
-		var ipzo = {'transparent': <?=$transparent?>, 'iscircle': <?=$iscircle?>, 'zoomLevel': <?=$zoom_level?>, 'zoomSize': <?=$zoom_size?>,'bg': "<?=plugins_url('/images/bg.jpg', __FILE__)?>"};
-	</script>
-	<?php
+    // enqueue style and script
+    wp_enqueue_style('Zoom-Product-Style', plugins_url('/css/Zoom-Product.css', __FILE__));
+    wp_enqueue_script('Zoom-Product-Script', plugins_url('/js/Zoom-Product.js', __FILE__), ['jquery'], '2.0.0', true);
 }
 
-add_action('wp_footer', "iplug_zoom_product_image");
+add_action('wp_enqueue_scripts', 'ipzo_frontend_assets');
+function ipzo_frontend_assets()
+{
+    // get plugin options
+    $ipzo = [
+        'transparency' => ipzo_get_option('_transparency', 0),
+        'isCircle' => ipzo_get_option('_is_circle', 1),
+        'zoomLevel' => ipzo_get_option('_zoom_level', 1.2),
+        'magnifierSize' => ipzo_get_option('_magnifier_size', 300),
+        'maxWidth' => ipzo_get_option('_max_width', 1),
+        'maxHeight' => ipzo_get_option('_max_height', 1),
+        'bg' => plugins_url('/images/bg.jpg', __FILE__)
+    ];
+
+    wp_localize_script('jquery', 'ipzo', $ipzo);
+}
 
 //Remove prettyPhoto lightbox
-add_action('wp_enqueue_scripts', 'iplug_remove_woo_lightbox', 99);
-function iplug_remove_woo_lightbox() {
-	wp_dequeue_style('woocommerce_prettyPhoto_css');
-	// wp_dequeue_script('prettyPhoto');
-	// wp_dequeue_script('prettyPhoto-init');
-	// wp_dequeue_script('flexslider');
-	// wp_dequeue_script('photoswipe');
-	// wp_dequeue_script('photoswipe-ui-default');
-	wp_dequeue_script('zoom');
+add_action('wp_enqueue_scripts', 'ipzo_remove_woo_lightbox', 99);
+function ipzo_remove_woo_lightbox()
+{
+    wp_dequeue_style('woocommerce_prettyPhoto_css');
+    // wp_dequeue_script('prettyPhoto');
+    // wp_dequeue_script('prettyPhoto-init');
+    // wp_dequeue_script('flexslider');
+    // wp_dequeue_script('photoswipe');
+    // wp_dequeue_script('photoswipe-ui-default');
+    wp_dequeue_script('zoom');
 }
 
+add_action('cmb2_admin_init', 'cmb2_ipzo_option_metabox');
 
-add_action('admin_menu', 'iplug_register_zoom_menu_page');
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_ipzo_option_metabox()
+{
+    // Start with an underscore to hide fields from custom fields list
+    $prefix = '_ipzo';
 
-function iplug_register_zoom_menu_page() {
-	$plugin_dir_path = dirname(__FILE__);
-	add_menu_page('Zoom Image', 'Zoom Image', 'manage_options', 'zoom-image', 'iplug_zoom_image_menu_page', plugins_url('/images/icon.svg', __FILE__), 6);
+    /**
+     * Initiate the metabox
+     */
+    $cmb = new_cmb2_box([
+        'id' => $prefix,
+        'title' => 'IPlug zoom Image',
+        'object_types' => ['options-page'],
+        'context' => 'normal',
+        'priority' => 'high',
+        'show_names' => true, // Show field names on the left
+        'option_key' => 'ipzo-wpm-options', // The option key and admin menu page slug.
+        'menu_title' => 'zoom Image',
+        'icon_url' => 'dashicons-search'
+    ]);
+
+    $cmb->add_field([
+        'name' => 'میزان بزرگنمایی',
+        'id' => $prefix . '_zoom_level',
+        'type' => 'text',
+        'default' => '1.2'
+    ]);
+
+    $cmb->add_field([
+        'name' => 'قطر ذره‌بین (پیکسل)',
+        'id' => $prefix . '_magnifier_size',
+        'type' => 'text',
+        'default' => '300'
+    ]);
+
+    $cmb->add_field([
+        'name' => ' حد اکثر عرض (پیکسل)',
+        'id' => $prefix . '_max_width',
+        'type' => 'text',
+        'default' => '800'
+    ]);
+
+    $cmb->add_field([
+        'name' => 'حداکثر ارتفاع (پیکسل)',
+        'id' => $prefix . '_max_height',
+        'type' => 'text',
+        'default' => '800'
+    ]);
+
+    $cmb->add_field([
+        'name' => 'شفافیت ذره‌بین',
+        'id' => $prefix . '_transparency',
+        'type' => 'radio_inline',
+        'options' => [
+            '1' => __('فعال', 'cmb2'),
+            '0' => __('غیرفعال', 'cmb2'),
+        ],
+        'default' => '1'
+    ]);
+
+    $cmb->add_field([
+        'name' => 'شکل ذره‌بین',
+        'id' => $prefix . '_is_circle',
+        'type' => 'radio_inline',
+        'options' => [
+            '1' => __('دایره', 'cmb2'),
+            '0' => __('مربع', 'cmb2'),
+        ],
+        'default' => '1'
+    ]);
 }
 
-function iplug_zoom_image_menu_page() {
-	?>
-	    <div class="wrap">
-	    <h1>IPlug zoom Image</h1>
-	    <form method="post" action="options.php">
-	        <?php
-				settings_fields("iplug-zoom-image");
-				do_settings_sections("iplug-zoom");
-				submit_button();
-			?>
-	    </form>
-		</div>
-	<?php
-}
-
-
-function iplug_display_zoom_level() {
-    echo '<input type="text"  type="number" name="iplug_zoom_level" id="iplug_zoom_level" value="'. get_option('iplug_zoom_level',1). '"/>';
-}
-
-function iplug_display_zoom_size() {
-    echo '<input type="text" name="iplug_zoom_size" id="iplug_zoom_size" value="' . get_option('iplug_zoom_size',300) . '" />';
-}
-
-function iplug_display_transparent() {
-	echo '<input type="checkbox" name="iplug_transparent" value="1"  ' . checked(1, get_option('iplug_transparent',1), false) .'/>';
-}
-
-function iplug_display_iscircle() {
-	echo '<input type="checkbox" name="iplug_iscircle" value="1" '. checked(1, get_option('iplug_iscircle',1), false) .'/>';
-}
-
-function iplug_display_addon_panel_fields() {
-	add_settings_section("iplug-zoom-image", "تنظیمات بزرگنمایی تصویر", null, "iplug-zoom");
-
-	add_settings_field("iplug_zoom_level", "Set Zoom Level", "iplug_display_zoom_level", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_zoom_size", "Set Zoom Width", "iplug_display_zoom_size", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_transparent", "Transparent", "iplug_display_transparent", "iplug-zoom", "iplug-zoom-image");
-	add_settings_field("iplug_iscircle", "Do you want show Circle ?", "iplug_display_iscircle", "iplug-zoom", "iplug-zoom-image");
-
-	register_setting("iplug-zoom-image", "iplug_zoom_level");
-	register_setting("iplug-zoom-image", "iplug_zoom_size");
-	register_setting("iplug-zoom-image", "iplug_transparent");
-	register_setting("iplug-zoom-image", "iplug_iscircle");
-}
-
-add_action("admin_init", "iplug_display_addon_panel_fields");
-
-add_action('admin_head', 'iplug_zoom_product_admin_style');
-function iplug_zoom_product_admin_style() {
-  echo	'<style>
-		    li#toplevel_page_zoom-image .wp-menu-image img {
-		      height: 100%;
-		      padding: 0;
-		    }
-		</style>';
+/**
+ * Wrapper function around cmb2_get_option
+ * @since  0.1.0
+ * @param  string $key     Options array key
+ * @param  mixed  $default Optional default value
+ * @return mixed           Option value
+ */
+function ipzo_get_option($key = '', $default = false)
+{
+    $prefix = '_ipzo';
+    $key    = $prefix . $key;
+    if (function_exists('cmb2_get_option')) {
+        // Use cmb2_get_option as it passes through some key filters.
+        return cmb2_get_option('ipzo-wpm-options', $key, $default);
+    }
+    // Fallback to get_option if CMB2 is not loaded yet.
+    $opts = get_option('ipzo-wpm-options', $default);
+    $val  = $default;
+    if ('all' == $key) {
+        $val = $opts;
+    } elseif (is_array($opts) && array_key_exists($key, $opts) && false !== $opts[$key]) {
+        $val = $opts[$key];
+    }
+    return $val;
 }
